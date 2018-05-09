5a617713ccd181f0dc4df8e5d27b04a6259b44a8
liu9293 <mylock.kai@gmail.com>
2018-05-09 19:06:42 +0800

    update

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,5 @@ export const mainnet = {
   url: 'https://mainnet.nebulas.io/v1',
   contract: 'n1y88MJ3FcBWK4rd9gRj8dqgJZk6VRcao3b'
 }
+
+// contract hash: e0864fd0fbb0c44193e6635aec2d8abd3e95d2b63b50dfea18da0825d6cba0c9
